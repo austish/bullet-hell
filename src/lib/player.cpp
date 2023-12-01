52a65d36e27262bac25307e29abb3f4e4c9abc83
austish <67724520+austish@users.noreply.github.com>
2023-11-30 16:05:47 -0800

    created makefile and separate player class

---
+++ src/lib/player.cpp
@@ -0,0 +1,39 @@
+#include "player.h"
+#include <cstring>
+
+Player::Player() {
+    // set all keyStates to 0
+    memset(keyStates, 0, sizeof(keyStates));
+
+    playerPosX = 0.0f;
+    playerPosY = 0.0f;
+    playerSize = 40.0f;
+    playerSpeed = 5.0f;
+}
+
+void Player::updatePlayer() {
+    if (keyStates['w'])
+        playerPosY += playerSpeed;
+    else if (keyStates['s'])
+        playerPosY -= playerSpeed;
+    else if (keyStates['a'])
+        playerPosX -= playerSpeed;
+    else if (keyStates['d'])
+        playerPosX += playerSpeed;
+}
+
+float Player::getPosX() {
+    return playerPosX;
+}
+
+float Player::getPosY() {
+    return playerPosY;
+}
+
+float Player::getSize() {
+    return playerSize;
+}
+
+void Player::updateKey(unsigned char key, bool value) {
+    keyStates[key] = value;
+}
