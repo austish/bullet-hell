ba9a35c3cb613f5de642f925e1a908e40a83d991
MeronDT <merondebt@gmail.com>
2023-12-01 02:25:42 -0800

    npc header

---
+++ src/lib/npc.h
@@ -0,0 +1,18 @@
+#ifndef NPC_H
+#define NPC_H
+
+class NPC{
+    private:
+        float NPCPosX;
+        float NPCPosY;
+        float NPCSize;
+        float NPCSpeed;
+    public:
+        NPC(float X, float Y, float size, float speed);
+        void updateNPC();
+        float getPosX();
+        float getPosY();
+        float getSize();
+};
+
+#endif
