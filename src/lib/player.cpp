74ad67d9cb62cdd6788507ef7ef0fd2540eed10a
austish <67724520+austish@users.noreply.github.com>
2023-12-03 20:18:44 -0800

    added boundaries for player

--- src/lib/player.cpp
+++ src/lib/player.cpp
@@ -1,24 +1,29 @@
 #include "player.h"
 #include <cstring>
 
+const extern float boundaryLeft = -400.0f;
+const extern float boundaryRight = 400.0f;
+const extern float boundaryTop = 300.0f;
+const extern float boundaryBottom = -300.0f;
+
 Player::Player() {
     //set all keyStates to 0
     memset(keyStates, 0, sizeof(keyStates));
 
     playerPosX = 0.0f;
     playerPosY = 0.0f;
-    playerSize = 40.0f;
-    playerSpeed = 5.0f;
+    playerSize = 30.0f;
+    playerSpeed = 3.0f;
 }
 
 void Player::updatePlayer() {
-    if (keyStates['w'])
+    if (keyStates['w'] && playerPosY < boundaryTop - playerSize / 2) 
         playerPosY += playerSpeed;
-    if (keyStates['s'])
+    if (keyStates['s'] && playerPosY > boundaryBottom + playerSize / 2) 
         playerPosY -= playerSpeed;
-    if (keyStates['a'])
+    if (keyStates['a'] && playerPosX > boundaryLeft + playerSize / 2) 
         playerPosX -= playerSpeed;
-    if (keyStates['d'])
+    if (keyStates['d'] && playerPosX < boundaryRight - playerSize / 2) 
         playerPosX += playerSpeed;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
+#ifdef __APPLE__ // Check if the target OS is macOS
 #define GL_SILENCE_DEPRECATION
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h> 
+#else
 #include <GL/glut.h>
+#endif
 #include "lib/player.h"
 #include "lib/npc.h"
+#include "lib/variables.h"
+
+//Keyboard button pressed
+void keyboardDown(unsigned char key, int x, int y);
+//Keyboard button released 
+void keyboardUp(unsigned char key, int x, int y);
+//Update function
+void update(int value);
 
 //Initialize player
 Player p;
@@ -18,10 +31,27 @@ void display() {
    glClear(GL_COLOR_BUFFER_BIT);
    glLoadIdentity();
 
+   // Draw boundaries (white)
+    glColor3f(1.0f, 1.0f, 1.0f); // Set boundary color to red
+    glBegin(GL_LINES);
+        // Top boundary
+        glVertex2f(boundaryLeft, boundaryTop);
+        glVertex2f(boundaryRight, boundaryTop);
+        // Bottom boundary
+        glVertex2f(boundaryLeft, boundaryBottom);
+        glVertex2f(boundaryRight, boundaryBottom);
+        // Left boundary
+        glVertex2f(boundaryLeft, boundaryBottom);
+        glVertex2f(boundaryLeft, boundaryTop);
+        // Right boundary
+        glVertex2f(boundaryRight, boundaryBottom);
+        glVertex2f(boundaryRight, boundaryTop);
+    glEnd();
+
    //Draw player (red)
-   glColor3f(1.0f, 0.0f, 0.0f);
+   glColor3f(1.0f, 0.0f, 0.0f);                       // Set color to red
+   glPushMatrix();                                    // Push player matrix
    float playerSize = p.getSize();
-   glPushMatrix();
    glTranslatef(p.getPosX(), p.getPosY(), 0.0f);
    glBegin(GL_QUADS);
    glVertex2f(-playerSize / 2, -playerSize / 2);
@@ -29,7 +59,7 @@ void display() {
    glVertex2f(playerSize / 2, playerSize / 2);
    glVertex2f(-playerSize / 2, playerSize / 2);
    glEnd();
-   glPopMatrix();
+   glPopMatrix();                                     // Pop player matrix
 
    //Draw Enemy NPCs (yellow)
    glColor3f(1.0f, 1.0f, 0.0f);
@@ -49,34 +79,12 @@ void display() {
    glutSwapBuffers();
 }
 
-//Update function
-void update(int value) {
-   p.updatePlayer();
-
-   for (int i = 0; i < sizeof(enemies) / sizeof(enemies[0]); ++i) {
-        enemies[i].updateNPC();
-   }
-
-   glutPostRedisplay();
-   glutTimerFunc(16, update, 0); // Approx 60 FPS
-}
-
-//Keyboard button pressed
-void keyboardDown(unsigned char key, int x, int y) {
-   p.updateKey(key, true);
-}
-
-//Keyboard button released 
-void keyboardUp(unsigned char key, int x, int y) {
-   p.updateKey(key, false);
-}
-
 int main(int argc, char** argv) {
    //Initialize window and game
    glutInit(&argc, argv);
    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-   glutInitWindowSize(1100, 800);
-   glutCreateWindow("Temporary name");
+   glutInitWindowSize(1200, 800);
+   glutCreateWindow("Temporary name");    //CHANGE NAME
 
    //Handle display and keyboard updates
    glutDisplayFunc(display);
@@ -94,3 +102,24 @@ int main(int argc, char** argv) {
    return 0;
 }
 
+//Keyboard button pressed
+void keyboardDown(unsigned char key, int x, int y) {
+   p.updateKey(key, true);
+}
+
+//Keyboard button released 
+void keyboardUp(unsigned char key, int x, int y) {
+   p.updateKey(key, false);
+}
+
+//Update function
+void update(int value) {
+   p.updatePlayer();
+
+   for (int i = 0; i < sizeof(enemies) / sizeof(enemies[0]); ++i) {
+        enemies[i].updateNPC();
+   }
+
+   glutPostRedisplay();
+   glutTimerFunc(16, update, 0); // Approx 60 FPS
+}
