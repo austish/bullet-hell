5ee85486d07e3c4e1e59c47cc88e984f6fc64632
emrecikisir <138903572+emrecikisir@users.noreply.github.com>
2023-12-07 13:45:34 -0800

    Update npc.h

--- src/lib/npc.h
+++ src/lib/npc.h
@@ -2,6 +2,10 @@
 #ifndef NPC_H
 #define NPC_H
 #include <string>
+#include <cmath>
+
+enum ShapeType { SQUARE, TRIANGLE, CIRCLE };
+enum NPCState { NORMAL, EVASIVE }; // State of the NPC
 
 class NPC{
     private:
@@ -10,10 +14,14 @@ class NPC{
         float size;
         float speed;
         int state; // 1 for alive, 0 for dead
+        NPCState state; // Updated to use NPCState enum
+        ShapeType shape; // Shape of the NPC
     public:
-        NPC(float X, float Y, float size, float speed);
-        void updateNPC();
+        NPC(float X, float Y, float size, float speed, ShapeType shapeType); 
+        void updateNPC(float playerPosX, float playerPosY); // Updated to take player's position
         void drawNPC();
+        // Method for collision detection
+        bool checkCollisionWithPlayer(float playerX, float playerY, float playerSize) const;
 };
 
 #endif
